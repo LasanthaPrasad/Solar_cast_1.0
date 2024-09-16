088b044a1857055ed9e0145088457d8f99b5c76a
LasanthaPrasad <ee_prasad@yahoo.com>
2024-09-16 19:29:23 +0530

    first commit

--- app/routes.py
+++ app/routes.py
@@ -72,8 +72,8 @@ import pandas as pd
 @main.route('/api/aggregate_grid_forecast')
 def aggregate_grid_forecast():
     now = datetime.now(timezone.utc)
-    start_time = now.replace(minute=0, second=0, microsecond=0) - timedelta(hours=12)
-    end_time = start_time + timedelta(hours=24)
+    start_time = now.replace(minute=0, second=0, microsecond=0) - timedelta(hours=1)
+    end_time = start_time + timedelta(hours=23)
 
     # Create a 30-minute resolution DataFrame
     date_range = pd.date_range(start=start_time, end=end_time, freq='30T')
